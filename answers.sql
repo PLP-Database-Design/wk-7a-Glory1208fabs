671b3f58313fb6dc6bdb998f4a923f6b86349a7e
Glory Fabowale <128857226+Glory1208fabs@users.noreply.github.com>
2025-05-02 04:44:03 +0100

    Update answers.sql

--- answers.sql
+++ answers.sql
@@ -7,3 +7,19 @@ FROM
     ProductDetail
 CROSS APPLY 
     STRING_SPLIT(Products, ',');
+
+QUESTION 2 ANSWER:
+-- Table 1: OrderDetailS
+CREATE TABLE Orders (
+    OrderID INT PRIMARY KEY,
+    CustomerName VARCHAR(100)
+);
+
+-- Table 2: OrderDetailS
+CREATE TABLE OrderDetails (
+    OrderID INT,
+    Product VARCHAR(100),
+    Quantity INT,
+    PRIMARY KEY (OrderID, Product),
+    FOREIGN KEY (OrderID) REFERENCES Orders(OrderID)
+);
