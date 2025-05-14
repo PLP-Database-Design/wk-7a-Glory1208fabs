c3df795a013328773578121eb8f6ddfc8861e417
Glory Fabowale <128857226+Glory1208fabs@users.noreply.github.com>
2025-05-14 13:42:00 +0100

    Update answers.sql

--- answers.sql
+++ answers.sql
@@ -1,36 +1,77 @@
-QUESTION 1 ANSWER:
-SELECT 
-    OrderID,
-    CustomerName,
-    TRIM(value) AS Product
-FROM 
-    ProductDetail
-CROSS APPLY 
-    STRING_SPLIT(Products, ',');
+-- Question 1
 
-QUESTION 2 ANSWER:
--- Table 1: Orders
-CREATE TABLE Orders (
-    OrderID INT PRIMARY KEY,
-    CustomerName VARCHAR(100)
-);
+CREATE TABLE ProductDetail (
 
--- Table 2: OrderDetailS
-CREATE TABLE OrderDetails (
     OrderID INT,
-    Product VARCHAR(100),
-    Quantity INT,
-    PRIMARY KEY (OrderID, Product),
-    FOREIGN KEY (OrderID) REFERENCES Orders(OrderID)
+
+    CustomerName VARCHAR(100),
+
+    Products VARCHAR(100)
+
 );
 
+INSERT INTO ProductDetail(OrderID, CustomerName, Products)
 
--- Insert into Orders 
+VALUES
+
+(101, 'John Doe', 'Laptop'),
+
+(101, 'John Doe', 'Mouse'),
+
+(102, 'Jane Smith', 'Tablet'),
+
+(102, 'Jane Smith', 'Keyboard'),
+
+(102, 'Jane Smith', 'Mouse'),
+
+(103, 'Emily Clark', 'Phone');
+
+-- Question 2
+
+CREATE TABLE Orders (
+
+    OrderID INT PRIMARY KEY,
+
+    CustomerName VARCHAR(100)
+
+);
+
 INSERT INTO Orders (OrderID, CustomerName)
-SELECT DISTINCT OrderID, CustomerName
-FROM OriginalOrderDetails;
 
--- Insert into OrderDetails
-INSERT INTO OrderDetails (OrderID, Product, Quantity)
-SELECT OrderID, Product, Quantity
-FROM OriginalOrderDetails;
+VALUES
+
+(101, 'John Doe'),
+
+(102, 'Jane Smith'),
+
+(103, 'Emily Clark');
+
+CREATE TABLE Product (
+
+    OrderID INT,
+
+    Product VARCHAR(100),
+
+    Quantity INT,
+
+    PRIMARY KEY (OrderID, Product),
+
+    FOREIGN KEY (OrderID) REFERENCES Orders(OrderID)
+
+);
+
+INSERT INTO Product (OrderID, Product, Quantity)
+
+VALUES
+
+(101, 'Laptop', 2),
+
+(101, 'Mouse', 1),
+
+(102, 'Tablet', 3),
+
+(102, 'Keyboard', 1),
+
+(102, 'Mouse', 2),
+
+(103, 'Phone', 1);
